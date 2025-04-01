8a06024f792103e2d8748ef13b28acd0f3427e57
Timo Wesselmann <Timo.Wesselmann@gmail.com>
2025-04-01 02:41:20 +0200

    fix: fixed project name

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule FontExtractor.MixProject do
+defmodule TTFonture.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :font_extractor,
+      app: :ttfonture,
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
