7a19fc56dc2109c698f2e2951467dd1d3d8e3b2c
Timo Wesselmann <timo.wesselmann@gmail.com>
2026-04-12 20:18:50 +0200

    update: added flake.nix and .envrc

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "A very basic flake";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShells.x86_64-linux.default =
+        pkgs.mkShell { packages = [ pkgs.beamMinimal28Packages.elixir_1_19 ]; };
+    };
+}
